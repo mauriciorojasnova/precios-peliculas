3d37a67a34309fbc63ff29406b99c3f445c138ee
mauriciorojasnova <andrsnov@gmail.com>
2021-08-11 22:35:40 -0500

    Añadí estilos al contenido principal

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-function calcular() {
+function compararPrecios() {
+  //Calcular precio de las boletas
   const input = document.getElementById("peliculas");
-  const value = input.value;
+  const value = Number(input.value);
   const precioUnitarioBoleta = 12000;
   const precioBoletas = value * precioUnitarioBoleta;
-  console.log(precioBoletas);
+
+  //Calcular precio de los servicios de streaming
+  let precioServicios;
+  const inputNetflix = document.getElementById("netflix");
+  const buyNetflix = inputNetflix.checked;
+  if (buyNetflix) {
+    precioNetflix = Number(inputNetflix.value);
+  } else {
+    precioNetflix = 0;
+  }
+
+  const inputAmazon = document.getElementById("amazon");
+  const buyAmazon = inputAmazon.checked;
+  if (buyAmazon) {
+    precioAmazon = Number(inputAmazon.value);
+  } else {
+    precioAmazon = 0;
+  }
+
+  const inputDisney = document.getElementById("disney");
+  const buyDisney = inputDisney.checked;
+  if (buyDisney) {
+    precioDisney = Number(inputDisney.value);
+  } else {
+    precioDisney = 0;
+  }
+
+  const inputHbo = document.getElementById("hbo");
+  const buyHbo = inputHbo.checked;
+  if (buyHbo) {
+    precioHbo = Number(inputHbo.value);
+  } else {
+    precioHbo = 0;
+  }
+
+  precioServicios = precioNetflix + precioAmazon + precioDisney + precioHbo;
+
+  //Comparar precios y enviar mensaje
+  if (precioServicios > precioBoletas) {
+    const mensajeFinal = document.getElementById("mensajeRecomendacion");
+    mensajeFinal.innerText = " Comprar las boletas de cine";
+  } else {
+    const mensajeFinal = document.getElementById("mensajeRecomendacion");
+    mensajeFinal.innerText = " Comprar los servicios de streaming";
+  }
+
 }
